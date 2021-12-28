b50e6f9aaa774619cd5dddc6f66ef071fa1ea055
Igor <igor.s.arkhipov@gmail.com>
2021-12-28 18:56:18 +0300

    VKClient app is being made, interface, animations, segues are built

---
+++ VKClient_AI/Controllers/AllGroupVC/AllGroupVC+SearchBarFunc.swift
@@ -0,0 +1,30 @@
+//
+//  File.swift
+//  VKClient_AI
+//
+//  Created by Игорь  Архипов on 09.12.2021.
+//
+
+import UIKit
+
+extension AllGroupVC: UISearchBarDelegate {
+    
+    func searchAllGroupsDelegate () {
+        searchAllGroupsBar.delegate = self
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        filteredAllGroupsArray = []
+        
+        if searchText.isEmpty{
+            filteredAllGroupsArray = allGroupsArray
+        }
+        for group in allGroupsArray {
+            if group.name.lowercased().contains(searchText.lowercased()) {
+                filteredAllGroupsArray.append(group)
+            }
+        }
+        self.tableView.reloadData()
+    }
+}
